7af14c0ca33ec71672a6cdd1d47422f9e26c3975
Haukur <haukurpalljonsson@gmail.com>
2016-12-18 17:09:34 +0100

    project with feature scaling

--- project/linear_regression.py
+++ project/linear_regression.py
@@ -1,56 +1,14 @@
-from math import exp, log, sqrt, pi
+from math import exp, log, sqrt, pi, pow
 from enum import Enum
+import numpy as np
 # We use the logarithm probability implementation provided by teachers
-import project.logarithms
-
-class NormalDistribution(object):
-    '''An implementation of the geometric distribution that whose support includes 0.'''
-
-    def __init__(self, median=0.0, variance=0.0):
-        '''Constructor
-
-        :param param: The parameter of this geometric distribution
-        :raises: ValueError if param is not in [0,1]
-        '''
-
-        self.median = median
-        self.std_dev = sqrt(variance)
-        self.constant = (sqrt(2.0*pi)*self.std_dev)
-
-    def log_prob(self, x):
-        '''Compute the log-probability of an observation
-
-        :param x: The observation
-        :return: The log-probability of x under this distribution
-        :raises: ValueError if x is not a non-negative integer
-        '''
-
-        if x % 1 != 0 or x < 0:
-            raise ValueError("x is not a non-negative integer")
-
-        return self.success + x * self.failure
-
-    def prob(self, x):
-        '''Compute the probability of an observation
-
-        :param x: The observation
-        :return: The probability of x under this distribution
-        :raises: ValueError if x is not a non-negative integer
-        '''
-        return exp(0.5*(pow((x-self.median)/self.std_dev, 2)))/self.constant
-
-    def get_param(self):
-        '''Get the parameter of this distribution
-
-        :return: The parameter of this distribution
-        '''
-        return self.median
 
 class Features(Enum):
     '''An enum to classify different feature types. Use skip=0 to not use a certain feature'''
     skip = 0
     continuous = 1
     categorical = 2
+    answer = 3
 #
 # Feature
 #     weight
@@ -60,7 +18,20 @@ class Features(Enum):
 class LinearRegression:
     '''A Class to represent the Linear Regression model.'''
 
-    def __init__(self, feature_selection=[Features.continuous, Features.continuous,Features.continuous],
+    def __init__(self, feature_selection=[Features.continuous,
+                                          Features.continuous,
+                                          Features.continuous,
+                                          Features.categorical,
+                                          Features.continuous,
+                                          Features.continuous,
+                                          Features.continuous,
+                                          Features.continuous,
+                                          Features.categorical,
+                                          Features.continuous,
+                                          Features.continuous,
+                                          Features.continuous,
+                                          Features.continuous,
+                                          Features.answer],
                  data_file_path='./data/data.txt'):
         '''Constructor
 
@@ -71,40 +42,118 @@ class LinearRegression:
         '''
         self.weights = []
         self.feature_selection = feature_selection
-        self.all_features = []
+        self.data = []
+        self.prices = []
+        self.SS_tot = 0.0
+        # learning rate
+        self.alpha = 0.005
+        self.categorical_features = dict()
         self.initialize(data_file_path)
 
-
-
     def initialize(self, data_file_path):
-        with open(data_file_path) as data:
-            for line in data:
-                features = line.split()
-                self.all_features.append(features)
-        # we initialize the weights - we just use the last line.
-        for x in range(len(features)):
-            self.weights[x] = 1.0
+        for index, feature in enumerate(self.feature_selection):
+            if feature == Features.categorical:
+                self.categorical_features[index] = dict()
+
+        raw_data = []
+        with open(data_file_path) as data_in:
+            for house_data in data_in:
+                # we consider all values as float
+                features = list(map(float, house_data.split()))
+                # we create our x_0 and set it to 1.
+                features.insert(0, 1.0)
+                self.prices.append(features.pop(len(features)-1))
+                raw_data.append(features)
+                for index, feature in enumerate(features):
+                    if index in self.categorical_features:
+                        if feature not in self.categorical_features[index]:
+                            self.categorical_features[index][feature] = -1
+
+        number_of_continuous_features = len(raw_data[0])
+        number_of_columns = len(raw_data[0])
+        for key in self.categorical_features.keys():
+            number_of_columns -= 1
+            number_of_continuous_features -= 1
+            number_of_columns += len(self.categorical_features[key].keys())
+
+        self.data = np.zeros(shape=(len(raw_data), number_of_columns))
+
+        for house_index, house_data in enumerate(raw_data):
+            # we skip the categoricals
+            index = 0
+            for feature_index, feature in enumerate(house_data):
+                if feature_index not in self.categorical_features:
+                    self.data[house_index][index] = feature
+                    index += 1
+        # we start from the same location
+        for house_index, house_data in enumerate(raw_data):
+            # we only take the categoricals
+            for feature_index, feature in enumerate(house_data):
+                if feature_index in self.categorical_features:
+                    # we havn't seen this one before
+                    if self.categorical_features[feature_index][feature] == -1:
+                        self.categorical_features[feature_index][feature] = index
+                        index += 1
+                    self.data[house_index][self.categorical_features[feature_index][feature]] = 1
+
+        # we do feature scaling
+        for x in range(1, number_of_continuous_features -1):
+            minimum = np.argmin(self.data[:, x])
+            maximum = np.argmax(self.data[:, x])
+            feature_mean = np.sum(self.data.T[x])/self.data.shape[0]
+            self.data[:, x] -= feature_mean
+            self.data[:, x] *= 1.0/(maximum - minimum)
+
+        for x in range(self.data.shape[1]):
+            self.weights.append(0.0)
+
+        mean_price = sum(self.prices)/len(self.prices)
+        for price in self.prices:
+            self.SS_tot += pow(price-mean_price, 2)
+
+        self.weights[0] = mean_price
+        # we cast our data structures to np data structures
+        self.weights = np.array(self.weights)
+        self.data = np.array(self.data)
 
     def train(self):
-        # feature * coeff = prediction (continuous features)
-        # feature (as categorical) |-> feature_value = ignore default (predictor_0 = 0)
-        # feature (as categorical) |-> feature_value = predictor_1 * ceoff_f1_1
-        # feature (as categorical) = 1
+        new_weights = []
+        summed_up = 0.0
+        for weight_index in range(self.weights.shape[0]):
+            for i in range(self.data.shape[0]):
+                summed_up += (self.predict(self.data[i])-self.prices[i])*self.data[i][weight_index]
+            new_weights.append(self.weights[weight_index] - self.alpha * summed_up/self.data.shape[0])
+        self.weights = np.array(new_weights)
 
-    def evaluate_model(self, data):
+    def predict(self, features):
+        '''
+
+        :param features: array of features
+        :param price:
+        :return:
+        '''
+        return np.dot(self.weights.T, features)
+
+    def evaluate_model(self):
         # go over data points and calculate residuals
         # model residuals with mean = 0
+        SS_res = 0.0
+        for i in range(len(self.data)):
+            f_i = self.predict(self.data[i])
+            SS_res += pow(self.prices[i] - f_i, 2)
+        return 1-(SS_res/self.SS_tot)
         # what is the variance?
         # R²=1-(sum(x_i-w.T*predict(x_i))²/sum(x_i-(1/n)*sum(x_i))² -> 1
 
 def main():
-    read_data()
-    train_on_data()
-    eval_error() #residuals
-    # variance of each normal distribution is expected to be the same but mean varies.
-    mean_of_dist_i = np.dot(weigth.T, predictions(x_i))
+    iterations = 300
+    lr = LinearRegression()
+    while iterations > 0:
+        lr.train()
+        iterations -= 1
+    value = lr.evaluate_model()
+    print(str(value))
+
 
 if __name__ == "__main__":
     main()
-
-mean_of_dist_i = np.dot(weigth.T, predictions(x_i))
