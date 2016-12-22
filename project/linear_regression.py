3763b0e0e9d772c8d73b92c51aa783abf1ce0adc
Haukur <haukurpalljonsson@gmail.com>
2016-12-22 23:45:47 +0100

    final version. basic probability programming

--- project/linear_regression.py
+++ project/linear_regression.py
@@ -2,6 +2,10 @@ from math import pow
 from enum import Enum
 import sys
 import numpy as np
+import matplotlib
+# this is due to a bug in linux
+matplotlib.use('Qt5Agg')
+from matplotlib import pyplot as plt
 
 
 # We use the logarithm probability implementation provided by teachers
@@ -11,6 +15,7 @@ class Features(Enum):
     skip = 0
     continuous = 1
     categorical = 2
+    value = 9
 
 
 class LinearRegression:
@@ -26,6 +31,7 @@ class LinearRegression:
         self.weights = []
         self.data = np.array([])
         self.actual_prices = []
+        self.value_index = []
 
         # learning rate
         self.alpha = 0.1
@@ -40,6 +46,8 @@ class LinearRegression:
                 self.categorical_features[feature_index] = list()
             elif feature == Features.skip:
                 self.skip_features.append(feature_index)
+            elif feature == Features.value:
+                self.value_index = [feature_index]
 
         self.initialize(data_file_path)
 
@@ -50,20 +58,22 @@ class LinearRegression:
         """
         raw_data = read_data_set(data_file_path)
         # we remove "y" from the raw data
-        self.actual_prices, raw_data = remove_prices(raw_data)
+        raw_data, self.actual_prices = remove_prices(raw_data, self.value_index[0])
         mean_price = sum(self.actual_prices) / len(self.actual_prices)
         for price in self.actual_prices:
             self.SS_tot += pow(price - mean_price, 2)
 
-        raw_data = remove_categorical_features(raw_data, self.categorical_features)
+        # we adjust the indieces of our lists
+        adjust_indieces(self.value_index, self.skip_features)
+        adjust_indieces(self.value_index, self.categorical_features)
+        raw_data, removed_keys = remove_categorical_features(raw_data, self.categorical_features)
+        # we need to adjust for the removed categorical features
+        adjust_indieces(removed_keys, self.skip_features)
+
         raw_data = remove_skip_features(raw_data, self.skip_features)
         self.continuous_feature_count = raw_data.shape[1]
         # we set the data with a column full of 1 last.
         self.data = np.append(raw_data, np.ones((len(raw_data), 1)), axis=1)
-
-        #self.data = np.append(self.data, np.power(self.data[:, 0].T, 2), axis=1)
-        #for index in range(self.continuous_feature_count):
-        #    self.data = np.append(self.data, np.power(self.data[:, index], 2), axis=1)
 
         for categorical_feature in self.categorical_features.keys():
             # add the dummy variables for the categoricals
@@ -71,8 +81,8 @@ class LinearRegression:
 
         # we do feature scaling, we skip the last continuous feature we added as 1 always
         for x in range(self.continuous_feature_count):
-            minimum = np.amax(self.data[:, x])
-            maximum = np.amin(self.data[:, x])
+            minimum = np.amin(self.data[:, x])
+            maximum = np.amax(self.data[:, x])
             feature_mean = np.sum(self.data[:, x]) / self.data.shape[0]
             self.data[:, x] -= feature_mean
             self.data[:, x] *= 1.0 / (maximum - minimum)
@@ -84,7 +94,7 @@ class LinearRegression:
         """ Run one iteration to get new weights
         """
         new_weights = []
-        # (X^T*X)^-1X^Ty
+        # gradient descent
         for weight_index in range(self.weights.shape[0]):
             summed_up = 0.0
             for i in range(self.data.shape[0]):
@@ -122,11 +132,23 @@ class LinearRegression:
         return self.weights
 
 
+def adjust_indieces(removed_keys, list_needs_adjusting):
+    """Adjust list index pointers
+
+    :param removed_keys: A list of indexes which have been removed
+    :param list_needs_adjusting: A list of indexes which need to be adjusted for elimination
+    """
+    for key in removed_keys:
+        for index, list_index in enumerate(list_needs_adjusting):
+            if key < list_index:
+                list_needs_adjusting[index] = list_index - 1
+
+
 def create_dummy_variables(categorical_features):
     """Break a categorical variable to multiple values
 
     :param categorical_features: An array of assignments the categorical value takes
-    :return: an array
+    :return: an array with a column for each value the categorical features takes
     """
     features = dict()
     index = 0
@@ -146,6 +168,12 @@ def create_dummy_variables(categorical_features):
 
 
 def remove_categorical_features(raw_data, categorical_variables):
+    """ Remove features from raw data
+
+    :param raw_data: The data
+    :param categorical_variables: A dictionary which states what indexes to use for categorical variables
+    :return raw data without the categorical variables, a list of removed features
+    """
     removed_keys = []
     for key in categorical_variables.keys():
         categorical_variables[key] = raw_data[:, key]
@@ -155,21 +183,40 @@ def remove_categorical_features(raw_data, categorical_variables):
     # we remove keys, the last one first
     for key in removed_keys:
         raw_data = np.delete(raw_data, key, axis=1)
-    return raw_data
+    return raw_data, removed_keys
 
 
 def remove_skip_features(raw_data, skip_features):
+    """ Remove features from raw data
+
+    :param raw_data: The data
+    :param skip_features: A list of indexes of features to not use
+    :return raw data without features listed
+    """
     skip_features.reverse()
     for index in skip_features:
-        raw_data = raw_data = np.delete(raw_data, index, axis=1)
+        raw_data = np.delete(raw_data, index, axis=1)
     return raw_data
 
 
-def remove_prices(raw_data):
-    return raw_data[:,  -1:], raw_data[:, :-1]
+def remove_prices(raw_data, index):
+    """ Remove prices from raw data
+
+    :param index: The index of the value
+    :param raw_data: The data
+    :return raw data without prices, prices
+    """
+    values = raw_data[:, index]
+    raw_data = np.delete(raw_data, index, axis=1)
+    return raw_data, values
 
 
 def read_data_set(data_file_path):
+    """Read file and return a numpy array
+
+    :param data_file_path: File path to data.txt so it can be read
+    ":return numpy array of the raw data
+    """
     raw_data = []
     with open(data_file_path) as data_in:
         for house_data in data_in:
@@ -184,32 +231,70 @@ def run_model(lr_model, iterations):
 
     :param lr_model: The model to train
     :param iterations: The number of iterations to run
-    :return:
+    :return: Returns two arrays: iteration, evaluation
     """
-    while iterations > 0:
+    i = 1
+    x = list()
+    y = list()
+    while iterations-i >= 0:
         lr_model.train()
-        iterations -= 1
         value = lr_model.evaluate_model()
+        i += 1
+        x.append(i)
+        y.append(value)
         print(str(value))
     print("Final weights: {}".format(lr_model.get_weights()))
+    return x, y
 
+
+def print_continuous_graph(graph, file_path, iterations, number_of_features, ):
+    """Create a graph s.t. it tries all features continuous
+
+    :param graph: The graph to use
+    :param file_path: To the data
+    :param iterations: Number of iterations to do gradient decent
+    :param number_of_features: The number of features in the dataset
+    """
+    for index in range(number_of_features):
+        feature_selection = [Features.skip] * number_of_features
+        feature_selection[index] = Features.continuous
+        feature_selection[13] = Features.value
+        lr = LinearRegression(feature_selection, file_path)
+        x, y = run_model(lr, iterations)
+        label = "{}, cont.".format(str(int(index+1)))
+        graph.plot(x, y, label=label)
+        graph.legend(loc='center left', bbox_to_anchor=(1, 0.5), fancybox=True, shadow=True)
 
 def main(arguments):
-    lr = LinearRegression([Features.skip,
-                           Features.skip, #2
-                           Features.skip,
-                           Features.skip,
-                           Features.skip, #5
-                           Features.skip,
-                           Features.skip,
-                           Features.skip,
-                           Features.skip, # 9
-                           Features.skip, # 10
-                           Features.skip, # 11
-                           Features.skip,
-                           Features.continuous],
-                          arguments[1])
-    run_model(lr, int(arguments[2]))
+    """Run the linear regression
+
+    :param arguments: Arguments from the command line. 1 is the data path. 2 is the number of iterations.
+    """
+    number_of_features = 14
+    file_path = arguments[1]
+    iterations = int(arguments[2])
+    graph = plt.subplot(111)
+    # print_continuous_graph(graph, file_path, iterations, number_of_features)
+    # use a list [Features.continuous, Features.continuous, Features.continuous, ...] to set the feature types to use.
+    feature_selection = [Features.skip] * number_of_features
+    feature_selection[1] = Features.continuous
+    feature_selection[2] = Features.value
+
+    lr = LinearRegression(feature_selection, file_path)
+    x, y = run_model(lr, iterations)
+    # plotting
+    label = "{}, cont.".format("3 (cat), \n6, 11 cont.")
+    graph.plot(x, y, label=label)
+    graph.legend(loc='center left', bbox_to_anchor=(1, 0.5), fancybox=True, shadow=True)
+
+    plt.ylabel('R²')
+    plt.xlabel('Iterations')
+    box = graph.get_position()
+    graph.set_position([box.x0, box.y0, box.width * 0.8, box.height])
+    plt.axis([0, int(arguments[2]), -1.00, 1.00])
+    plt.grid(True)
+    plt.savefig('out.png')
+    plt.show()
 
 
 if __name__ == "__main__":
--- project/readme.md
+++ project/readme.md
@@ -3,4 +3,15 @@ We use python version 3. Please adjust command to your environment.
 ```
 python3 linear_regression.py ./data/data.txt 300
 ```
-Where ./data/data.txt is the path to the data and 300 is the number of iterations you would like to do. 
+Where './data/data.txt' is the path to the data and '300' is the number of iterations you would like to do.
+The program can be adjusted in the code to run it with different feature selections. See code for further explanation.
+## Requirements to run
+numpy and matplotlib installed. These are not required by the model to run so the code can be adjusted to remove these requirements without breaking the linear regression model itself.
+
+# Contents of file
+* lr.py is the code for the linear regression model
+* readme.md is this file
+* report.pdf is the report of the project
+
+# Data
+[Here- Housing](https://archive.ics.uci.edu/ml/datasets/Housing)
